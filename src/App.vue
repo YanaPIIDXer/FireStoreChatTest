8f884c5aec91031c12e6a55925b35d49363345f2
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-10-06 06:01:12 +0900

    Firestoreをセットアップして購読準備

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,17 @@
 <script setup lang="ts">
+import { onUnmounted } from 'vue';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from '@/assets/firebase.json';
+import { getFirestore, doc, onSnapshot, DocumentSnapshot } from 'firebase/firestore';
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+const unsub = onSnapshot(doc(db, 'chat', 'message'), (snapshot: DocumentSnapshot) => {
+  console.log(snapshot);
+});
+
+onUnmounted(() => unsub);
 </script>
 
 <template lang="pug">
