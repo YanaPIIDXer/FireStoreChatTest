3132822433aa6988bf33e40d9551376675b2e644
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-10-06 06:17:30 +0900

    チャット送信処理実装

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,54 @@
 <script setup lang="ts">
-import { onUnmounted } from 'vue';
+import { ref, onUnmounted } from 'vue';
 import { initializeApp } from 'firebase/app';
 import firebaseConfig from '@/assets/firebase.json';
-import { getFirestore, doc, onSnapshot, DocumentSnapshot } from 'firebase/firestore';
+import { getFirestore, query, onSnapshot, DocumentSnapshot, addDoc, collection, QuerySnapshot } from 'firebase/firestore';
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 
-const unsub = onSnapshot(doc(db, 'chat', 'message'), (snapshot: DocumentSnapshot) => {
+interface Message {
+  id: number;
+  message: string;
+}
+const messages = ref<Message[]>([]);
+const sendMessage = ref('');
+
+const unsub = onSnapshot(query(collection(db, 'chat')), (snapshot: QuerySnapshot) => {
   console.log(snapshot);
 });
 
 onUnmounted(() => unsub);
+
+/**
+ * 送信
+ */
+const onSend = async (e: Event) => {
+  e.preventDefault();
+
+  await addDoc(collection(db, 'chat'), {
+    id: messages.value.length + 1,
+    message: sendMessage.value,
+  });
+
+  sendMessage.value = '';
+}
 </script>
 
 <template lang="pug">
 .root
   h1 チャット
+  hr
+  table(border='1')
+    thead
+      tr
+        th Message
+    tbody
+      tr(v-for="(msg, index) in messages", :key="index")
+        td {{ message }}
+  hr
+  form(@submit="onSend")
+    input(type='text' v-model="sendMessage")
 </template>
 
 <style lang="sass" scoped>
